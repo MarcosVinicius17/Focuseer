4b90716fbd3176f2ad9009987ec63924d1f5a953
MarcosVinicius17 <vieiramarcos35@hotmail.com>
2023-06-06 18:20:46 -0300

    A lot of changes

--- gtk_implementation/login.py
+++ gtk_implementation/login.py
@@ -5,7 +5,8 @@ from gi.repository import Gtk
 
 
 def login(button):
-    print("login")
+    print(txtLogin.get_text())
+    print(f"password:", txtSenha.get_text())
 
 
 def cadastro(button):
@@ -26,6 +27,28 @@ btnLogin.connect("clicked", login)
 
 btnCadastro = builder.get_object("btnCadastro")
 btnCadastro.connect("clicked", cadastro)
+
+
+css_provider = Gtk.CssProvider()
+css_provider.load_from_path(
+    "/home/marcos/Desktop/UNIP/tcc/gtk_implementation/login_css.css"
+)
+
+
+context2 = btnCadastro.get_style_context()
+context_window = window.get_style_context()
+context3 = btnLogin.get_style_context()
+
+
+context4 = txtLogin.get_style_context()
+# context.add_provider(css_provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
+
+context_window.add_provider(css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+context2.add_provider(css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+context3.add_provider(css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+
+
+context4.add_provider(css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
 window.show_all()
 
