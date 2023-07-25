8811c08e34585c8f7cccc4739c4ce1a394bcceb5
MarcosVinicius17 <vieiramarcos35@hotmail.com>
2023-07-25 17:37:05 -0300

    QoL updates

---
+++ gtk_implementation/data_analysis.py
@@ -0,0 +1,19 @@
+"""'
+Contem os metodos que analisam os dados do usuario (principalmente os provenientes do monitor de processos) para a criacao de graficos e estatisticas
+"""
+
+
+def time_spend_working():
+    return
+
+
+def blacklister_process_time():
+    return
+
+
+def whitelisted_process_time():
+    return
+
+
+def achieved_goal_rate():
+    return
