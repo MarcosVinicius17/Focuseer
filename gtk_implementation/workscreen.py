0c5003c729e01dd34894e0e6ac3e93e102a215df
MarcosVinicius17 <vieiramarcos35@hotmail.com>
2023-08-21 16:47:57 -0300

    qol

--- gtk_implementation/workscreen.py
+++ gtk_implementation/workscreen.py
@@ -6,15 +6,61 @@ from deepdiff import DeepDiff
 
 """
 https://pypi.org/project/tqdm/
-caso vc decida voltar a usar uma barra de progresso
-"""
-"""
-implementar o metodo para encerrar o dia, e ele precisa "zerar" o json
+barra de progresso
 """
 
 
-def encerrar_dia() -> None:
-    pass
+def esvaziar_json():
+    with open("gtk_implementation/temp_data.json", "r") as file:
+        data = json.load(file)
+
+    def empty_recursive(obj):
+        if isinstance(obj, dict):
+            for key in obj:
+                obj[key] = empty_recursive(obj[key])
+        elif isinstance(obj, list):
+            obj.clear()
+        elif isinstance(obj, bool):
+            obj = False
+        else:
+            obj = None
+        return obj
+
+    new_data = empty_recursive(data)
+
+    with open("gtk_implementation/temp_data.json", "w") as file:
+        json.dump(new_data, file, indent=4)
+
+
+"""
+1) "coletar" os checkboxes e verificar a % de quantos foram completos
+2) esvaziar o json (limpar os campos)
+3) obter data dos processos (tempo gasto)
+4) gerar relatório (passar valores para o .py responsavel)
+"""
+
+
+def encerrar_dia(button) -> None:
+    total_items = 0
+    checked_items = 0
+    checked = []
+    unchecked = []
+
+    for child in boxObjetivos.get_children():
+        if isinstance(child, Gtk.CheckButton):
+            total_items += 1
+            if child.get_active():
+                checked_items += 1
+                checked.append(child.get_label())
+            else:
+                unchecked.append(child.get_label())
+    print(f"checked: {checked}")
+    print(f"unchecked: {unchecked}")
+    if total_items == 0:
+        return 0.0
+    else:
+        percentage = (checked_items / total_items) * 100
+        print("completion:", percentage)
 
 
 def get_hora_final() -> str:
@@ -134,7 +180,6 @@ def add_item_objetivos(button) -> None:
             # Temporarily remove the "Add Item" button
             boxObjetivos.remove(button)
 
-            # Add the new check button
             boxObjetivos.add(check_button)
 
             # Re-add the "Add Item" button at the end
@@ -264,8 +309,10 @@ lblProgresso = builder.get_object("lblProgresso")
 btnObjetivos.connect("clicked", add_item_objetivos)
 window.connect("realize", set_tempo_trabalho)
 btnHome.connect("clicked", open_home)
-btnEncerrar = builder.get_object("btnEncerrar")
 
+
+btnEncerrar = builder.get_object("btnEncerrar")
+btnEncerrar.connect("clicked", encerrar_dia)
 
 css_provider = Gtk.CssProvider()
 css_provider.load_from_path("gtk_implementation/custom_colors.css")
